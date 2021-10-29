47f0eb9ed74bea613e69024f016dae1cc3b80941
AidanKLee <93386628+AidanKLee@users.noreply.github.com>
2021-10-29 14:05:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10)
+};
+
+const getAbsoluteDistance = (player, target) => {
+  let result;
+  if (player >= target) {
+    return player - target;
+  } else {
+    return target - player;
+  }
+}
+
+const compareGuesses = (human, pc, target) => {
+  if (human < 0 || human > 9) {
+    alert("You must enter a number between 0 and 9. Computer Wins!")
+  } else {
+    human = getAbsoluteDistance(human, target);
+  pc = getAbsoluteDistance(pc, target);
+  if (human <= pc) {
+    return true;
+  } else {
+    return false;
+    }
+  }
+};
+
+const updateScore = winner => {
+  if (winner === 'human') {
+    return humanScore ++;
+  } else {
+    return computerScore ++;
+  }
+};
+
+const advanceRound = () =>  currentRoundNumber ++;
